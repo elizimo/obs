cb64f9ff4acb64bc47d3fa8f63d5fdfe473c29fe
elizimo <elizaku797@gmail.com>
2026-04-21 04:22:07 +0000

    Removal due to redundant

--- src/data/absdata.json.js
+++
@@ -1,9 +0,0 @@
-async function json(url) {
-  const response = await fetch(url);
-  if (!response.ok) throw new Error(`fetch failed: ${response.status}`);
-  return await response.json();
-}
-
-const data = await json(`https://data.api.abs.gov.au/rest/data/AUSTRALIAN_INDUSTRY/all?format=jsondata`);
-
-process.stdout.write(JSON.stringify(data));
